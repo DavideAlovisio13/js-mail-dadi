45c0c69c9b823ab74dff471d2301a197b90760aa
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-11 15:19:29 +0100

    (js) add 1 exercise

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 
 Gioco dei dadi
+
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
@@ -21,4 +22,48 @@ Consigli del giorno:
 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 Buon lavoro e buon divertimento!:
 P.S Dadi
+
 */
+
+// 1 === MAIL
+
+// dichiariamo una nuova variabile dove andremo a rchiamare il bottone 
+
+const submitBtn = document.querySelector('.submitbtn')
+
+// inziamo una funzione dove inseriamo un ascoltatore che al click del bottone confronti e controlli i dati inseriti
+submitBtn.addEventListener('click', function() {
+
+    // creiamo un Array contenente le mail di chi può accedere 
+    let serverUsers = [
+        'AlanParrish@jumanji.com',
+        'SarahWhittle@jumanji.com', 
+        'SarahWhittle@jumanji.com',
+        'JudyShepherd@jumanji.com',
+        'Peter Shepherd@jumanji.com',
+    ];
+
+    // creiamo una variavile contenente il valore inserito dall'utente tramite imput
+    let userValue = document.getElementById('emailinput').value;
+
+    // creiamo una variabile booleana per controllare se l'utente può accedere oppure no
+    let accessAllowed = false;
+
+    // controlliamo, a questo putno, che l'utente possa accedere
+
+    for(let i = 0; i < serverUsers.length; i++) {
+        if(userValue.toLowerCase()  === serverUsers[i].toLowerCase()) {
+            accessAllowed = true;
+        }
+    }
+    // inviamo adesso all'utente un messaggio per confermare o negare l'accesso
+
+    let messUser = document.getElementById('messagexuser');
+        if(accessAllowed) {
+            messUser.innerHTML = 'Acesso consentito'
+        } else {
+            messUser.innerHTML = 'Accesso negato'
+        }
+})
+
+// 2 === gioco dei dadi 
